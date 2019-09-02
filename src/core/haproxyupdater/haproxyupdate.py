ed3d1b9e1c5c8962001488173e9af12a9e13bdf8
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-02 12:10:23 +0530

    Add class for reloading haproxy

---
+++ src/core/haproxyupdater/haproxyupdate.py
@@ -0,0 +1,14 @@
+import os
+
+class HaproxyUpdate(object):
+    def __init__(self, **kwargs):
+        pass
+
+    def __sanitise(self):
+        return True
+
+    def update_haproxy(self):
+        pass
+
+    def reload_haproxy(self):
+        pass
