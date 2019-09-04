a31b3170aa28ef1544b49dc9c3f30473345a3d94
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-04 23:16:32 +0530

    Add method to create socket

--- src/core/haproxyupdater/sockethandler.py
+++ src/core/haproxyupdater/sockethandler.py
@@ -6,3 +6,21 @@ class SocketHandler(object):
 
         # get the desired params
         self.sock_file = kwargs.get("sock_file")
+
+    def create_socket(self):
+        self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.socket.settimeout(10)
+
+        try:
+
+            # try connecting to haproxy socket file
+            self.socket.connect(self.sock_file)
+        except Exception:
+
+            '''
+                Log exception
+            '''
+
+            return False
+
+        return True
