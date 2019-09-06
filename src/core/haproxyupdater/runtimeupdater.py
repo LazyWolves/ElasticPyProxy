a9e39a0702bd9954eb9a16eead8282d83aaa473a
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-06 23:17:27 +0530

    Add method to get status and update haproxy runtime

--- src/core/haproxyupdater/runtimeupdater.py
+++ src/core/haproxyupdater/runtimeupdater.py
@@ -31,10 +31,39 @@ class RuntimeUpdater(object):
                     active_nodes[node_addr] = node_name
 
         nodes = {
-            "active": active_nodes,
+            "active_nodes": active_nodes,
             "inactive_nodes": inactive_nodes
         }
 
         return True, nodes
-            
 
+    @staticmethod
+    def update_runtime_util(haproxy_sock, node_ips, nodes):
+        active_nodes = nodes.get("active_nodes")
+        inactive_nodes = nodes.get("inactive_nodes")
+
+    @staticmethod
+    def update_haproxy_runtime(**kwargs):
+        node_ips = kwargs.get("nodes")
+        port = kwargs.get("port")
+        sock_file = kwargs.get("sock_file")
+        backend_name = kwargs.get("node_name")
+
+        socketHandler = SocketHandler(sock_file=sock_file)
+
+        socket_created = socketHandler.create_socket()
+
+        if not socket_created:SocketHandler
+            return False
+
+        got_status, nodes = RuntimeUpdater.__get_haproxy_stats(socketHandler, backend_name)
+
+        if not got_status:
+            return False
+
+        updated = RuntimeUpdater.update_runtime_util(socketHandler, node_ips, nodes)
+
+        if not updated:
+            return False
+
+        return True
