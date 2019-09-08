1d973e177cc8d32c76309d4f46f88c45cb4837f0
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-08 23:37:15 +0530

    Add method to get instance ids

--- src/core/nodefetchers/awsfetcher/botohandler.py
+++ src/core/nodefetchers/awsfetcher/botohandler.py
@@ -14,3 +14,31 @@ class BotoHandler(object):
         )
 
         return client
+
+    @staticmethod
+    def get_ec2_client(**kwargs):
+        aws_access_key_id = kwargs.get("aws_access_key_id")
+        aws_secret_access_key = kwargs.get("aws_secret_access_key")
+
+        client = boto3.client(
+            "ec2",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
+
+        return client
+
+    @staticmethod
+    def get_instance_ids_for_asg(boto_client, asg_name):
+        response = boto_client.describe_auto_scaling_groups(
+            AutoScalingGroupNames=[
+                asg_name
+            ]
+        )
+
+        instances = response.get("AutoScalingGroups")[0]["Instances"]
+
+        instance_ids = []
+
+        for instance in instances:
+            instance_ids.append(instance.get("InstanceId"))
