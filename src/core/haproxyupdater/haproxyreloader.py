984d139de81e64e3214ad605a302de1b9ff09416
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-08 16:11:23 +0530

    Add condition for reloading via binary

--- src/core/haproxyupdater/haproxyreloader.py
+++ src/core/haproxyupdater/haproxyreloader.py
@@ -11,3 +11,13 @@ class HaproxyReloader(object):
             service_name = kwargs.get("service_name")
 
             reloaded = HaproxyReloader.reload_by_systemd(service_name)
+
+            return reloaded
+
+        else if start_by == "binary":
+            binary = kwargs.get("haproxy_binary")
+            haproxy_config_file = kwargs.get("haproxy_config_file")
+
+            reloaded = HaproxyReloader.reload_by_binary(binary, haproxy_config_file)
+
+            return reloaded
