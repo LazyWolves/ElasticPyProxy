cf274d82695e6961705e4474d14cfe665ded2adb
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-08 16:40:20 +0530

    Add shell executer method

--- src/core/haproxyupdater/haproxyreloader.py
+++ src/core/haproxyupdater/haproxyreloader.py
@@ -10,7 +10,7 @@ class HaproxyReloader(object):
         if start_by == "systemd":
             service_name = kwargs.get("service_name")
 
-            reloaded = HaproxyReloader.reload_by_systemd(service_name)
+            reloaded = HaproxyReloader.__reload_by_systemd(service_name)
 
             return reloaded
 
@@ -18,14 +18,31 @@ class HaproxyReloader(object):
             binary = kwargs.get("haproxy_binary")
             haproxy_config_file = kwargs.get("haproxy_config_file")
 
-            reloaded = HaproxyReloader.reload_by_binary(binary, haproxy_config_file)
+            reloaded = HaproxyReloader.__reload_by_binary(binary, haproxy_config_file)
 
             return reloaded
 
     @staticmethod
-    def reload_by_systemd(service_name):
+    def __reload_by_systemd(service_name):
         pass
 
     @staticmethod
-    def reload_by_binary(service_name, haproxy_config_file):
+    def __reload_by_binary(service_name, haproxy_config_file):
         pass
+
+    @staticmethod
+    def __execute_shell(command):
+
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        output, errors = proc.communicate()
+        proc_exit_code = proc.returncode
+
+        if proc_exit_code != 0:
+
+            '''
+                Log the error and the exit code
+            '''
+
+            return False
+
+        return True
