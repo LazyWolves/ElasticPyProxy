a8d39072512000fe77864a7a7dd614fea41ae909
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-09 22:22:46 +0530

    Add method for fectching aws asg ips

--- src/core/nodefetchers/awsfetcher/awsfetcher.py
+++ src/core/nodefetchers/awsfetcher/awsfetcher.py
@@ -1,13 +1,30 @@
 import os
 import requests
 from src.core.nodefetchers.basefetcher import BaseFetcher
+from .botohandler import BotoHandler
 
 class AwsFetcher(object):
     def __init__(self, **kwargs):
-        pass
+        self.aws_access_key_id = kwargs.get("aws_access_key_id")
+        self.aws_secret_access_key = kwargs.get("aws_secret_access_key")
+        self.asg_name = kwargs.get("asg_name")
+        self.ip_type = kwargs.get("ip_type")
+
+        self.asg_boto_client = BotoHandler.get_auto_scaling_client(aws_access_key_id=self.aws_access_key_id,
+                                                                   aws_secret_access_key=self.aws_secret_access_key
+                                                                )
+        self.ec2_boto_client = BotoHandler.get_ec2_client(aws_access_key_id=self.aws_access_key_id,
+                                                          aws_secret_access_key=self.aws_secret_access_key
+                                                        )
 
     def __check_response(self):
         return True
 
     def fetch(self):
-        return []
+        asg_instance_ips = BotoHandler.get_instance_ips_for_asg(asg_client=self.asg_boto_client,
+                                                                ec2_client=self.ec2_boto_client,
+                                                                asg_name=self.asg_name,
+                                                                ip_type=self.ip_type
+                                                            )
+
+        return asg_instance_ips
