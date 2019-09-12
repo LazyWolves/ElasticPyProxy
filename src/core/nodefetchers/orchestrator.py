028de7d5324b56e60fc7631d6483cfbe05bb7ad1
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-13 00:32:05 +0530

    Updater orchestrator function

--- src/core/nodefetchers/orchestrator.py
+++ src/core/nodefetchers/orchestrator.py
@@ -1,10 +1,21 @@
 from .awsfetcher.awsfetcher import AwsFetcher
 
-ORCHESTRATOR_HANDLERS = {
-    "aws": AwsFetcher
-}
+def get_orchestrator_handler(config):
+    orchestrator = config.get("orchestrator")
 
-def get_orchestrator_handler(orchestator):
-    orchestrator = orchestator.lower()
+    if orchestrator.lower() == "aws":
+        aws_handler = prepare_aws_handler(config.get(orchestrator))
 
-    return ORCHESTRATOR_HANDLERS.get(orchestrator)
+def prepare_aws_handler(config):
+    aws_access_key_id = config.get("aws_access_key_id")
+    aws_secret_access_key = config.get("aws_secret_access_key")
+    ip_type = config.get("ip_type")
+    asg_name = config.get("asg_name")
+
+    aws_handler = AwsFetcher(aws_access_key_id=aws_access_key_id,
+                             aws_secret_access_key=aws_secret_access_key,
+                             ip_type=ip_type,
+                             asg_name=asg_name
+                            )
+
+    return aws_handler
--- src/driver/bootstrap.py
+++ src/driver/bootstrap.py
@@ -1,7 +1,12 @@
 from src.core.haproxyupdater.haproxyupdate import HaproxyUpdate
 from src.core.nodefetchers.awsfetcher.awsfetcher import AwsFetcher
+from src.core.nodefetchers.orchestrator import get_orchestrator_handler
 
 def bootstrap(**kwargs):
     config = kwargs.get("config")
+
+    orchestrator = config.get("orchestrator")
+
+    orchestratorHandler = get_orchestrator_handler(config)
 
     
