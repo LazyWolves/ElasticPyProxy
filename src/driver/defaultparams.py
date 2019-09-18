32dda6d2b839356eb8783f576b074befaa6f3266
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-18 09:59:55 +0530

    Add default params

--- src/driver/defaultparams.py
+++ src/driver/defaultparams.py
@@ -1,7 +1,18 @@
 default_params = {
-    "CONFIG_FILE" : "ep2.config",
-    "LOCK_FILE" : "ep2.lock",
-    "SLEEP_BEFORE_NEXT_RUN" : 300,
-    "SLEEP_BEFORE_NEXT_LOCK_ATTEMPT" : 300,
-    "LOG_FILE" : "/var/log/ep2/ep2.log"
+    "haproxy_config_file" : "/etc/haproxy/haproxy.cfg",
+    "template_file" : "/etc/ep2/ep2.template",
+    "backend_port" : 6003,
+    "haproxy_binary" : "/usr/sbin/haproxy",
+    "start_by" : "binary",
+    "haproxy_socket_file" : "/var/run/haproxy/haproxy.sock",
+    "pid_file" : "/run/haproxy.pid",
+    "backend_name" : "haproxynode",
+    "update_type" : "update_by_runtime",
+    "node_slots" : 50,
+    "service_name" : "haproxy",
+    "lock_dir" : "/var/run/ep2",
+    "orchestrator" : "aws",
+    "sleep_before_next_run" : 5,
+    "sleep_before_next_lock_attempt" : 5,
+    "log_file" : "/var/log/ep2/ep2.log"
 }
