cb5585cb4b9d0cd634035baf2e9dfdf441690737
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-09-22 18:56:07 +0530

    Add doc for aws fetcher

--- src/core/nodefetchers/orchestrator.py
+++ src/core/nodefetchers/orchestrator.py
@@ -1,6 +1,26 @@
+"""
+.. module:: orchestrator
+   :synopsis: Module for initialising backend fetcher
+
+"""
+
 from .awsfetcher.awsfetcher import AwsFetcher
 
 def get_orchestrator_handler(config, logger=None):
+
+    """ Method for deciding which fetcher to use
+
+        Decide which fetcher to use depending on the orchestrator mentioned
+        in the config.
+
+        Args:
+            config (dictionary) : dictionary holding ep2 config
+            logger (object) : logger object
+
+        Returns:
+            object : Backend fetcher
+
+    """
     orchestrator = config.get("haproxy").get("orchestrator")
 
     if orchestrator.lower() == "aws":
@@ -9,6 +29,15 @@ def get_orchestrator_handler(config, logger=None):
     return handler
 
 def prepare_aws_handler(config):
+
+    """ Prepares the AWS fetcher
+
+        Args:
+            config (dictionary) : dictionary containing ep2 config
+
+        Returns:
+            srv.nodefetchers.awsfetcher.awsfetcher : Aws backend fetecher
+    """
     aws_access_key_id = config.get("aws_access_key_id")
     aws_secret_access_key = config.get("aws_secret_access_key")
     ip_type = config.get("ip_type")
